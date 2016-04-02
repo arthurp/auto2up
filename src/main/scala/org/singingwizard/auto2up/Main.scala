b650434758028d9380e5b41e13b426e8e8a26883
Arthur Peters <amp@singingwizard.org>
2016-04-01 22:12:34 -0500

    Reorganize code and comment. Add proper output filename computation.
    Fixes #1

--- src/main/scala/org/singingwizard/auto2up/Main.scala
+++ src/main/scala/org/singingwizard/auto2up/Main.scala
@@ -33,7 +33,7 @@ class Conf(args: Array[String]) extends ScallopConf(args) {
   mainOptions = Seq(outputFilename)
   val inputFilename = trailArg[String]("input", descr = "Input filename (really required, BUG)", required = false)
   val outputFilename = opt[String]("output", descr = "Output filename",
-    default = inputFilename.get.map(i => i.stripSuffix(".pdf") + "-2up.pdf"))
+    default = inputFilename.get.map(outputFilenameForFilename))
 
   val drawBox = opt[Boolean]("draw-box", 'b', descr = "Draw boxes around placed pages", default = Some(false))
 
@@ -42,10 +42,10 @@ class Conf(args: Array[String]) extends ScallopConf(args) {
     default = Some(0.95f))
   val dpi = opt[Float]("dpi", 'd',
     descr = "Rendering resolution used for analysis in DPI.",
-    default = Some(100f))
+    default = Some(60f))
   val inkMargin = opt[Float]("ink-margin", 'n',
     descr = "Margin to leave around ink when cropping in points.",
-    default = Some(2f))
+    default = Some(3f))
 
   val shortEdgeMargin = opt[Float]("short-edge-margin", 's',
     descr = "Margin on short paper edge in points.",
@@ -56,9 +56,23 @@ class Conf(args: Array[String]) extends ScallopConf(args) {
   val interMargin = opt[Float]("inter-margin", 'i',
     descr = "Gap between pages placed on one sheet in points.",
     default = Some(9f / 2))
+    
+  val verbose = tally("verbose", descr = "Increase verbosity")
+  val quiet = tally("quiet", descr = "Decrease verbosity")
+  
+  def verbosity = verbose() - quiet()
 
   val help = opt[Boolean]("help", descr = "Show this help", default = Some(false))
   verify()
+
+  /** Remove the extension from a filename and replace it with "-2up.pdf".
+    */
+  def outputFilenameForFilename(fn: String): String = {
+    val nameStart = fn.lastIndexOf(File.separator) max 0
+    val extStart = fn.indexOf('.', nameStart)
+    val sansExt = if (extStart < 0) fn else fn.substring(0, extStart)
+    sansExt + "-2up.pdf"
+  }
 }
 
 object Main {
@@ -71,142 +85,126 @@ object Main {
         conf.printHelp()
       }
     } catch {
-      case ScallopException(msg) =>
+      case ScallopException(msg) ⇒
         println(msg)
     }
   }
   def main(conf: Conf): Unit = {
+    implicit val _conf = conf
+
+    // Extract margin config for later use
+    val shortEdgeMargin = conf.shortEdgeMargin()
+    val longEdgeMargin = conf.longEdgeMargin()
+    val interMargin = conf.interMargin()
+
+    // The output page size
+    val pageSize = new PDRectangle(PDRectangle.LETTER.getHeight, PDRectangle.LETTER.getWidth)
+
+    // The amount of the page we will actually use.
+    val usedSize = new PDRectangle(pageSize.getWidth - shortEdgeMargin * 2 - interMargin, pageSize.getHeight - longEdgeMargin * 2)
+
+    // Load/open PDFs
     val inputFile = new File(conf.inputFilename())
     val outputFile = new File(conf.outputFilename())
+
+    if (!(inputFile.isFile() && inputFile.canRead()))
+      throw new IOException("Input file not readable")
+    if (!outputFile.canWrite() || (outputFile.exists() && !outputFile.isFile()))
+      throw new IOException("Output file not writable (check permissions and what is there now)")
+    if (outputFile.exists())
+      trace(-1, s"$outputFile already exists. Overwriting.")
+
+    trace(0, s"Input file: $inputFile")
     for {
-      in <- managed(loadInput(inputFile))
-      out <- managed(new PDDocument())
+      in ← managed(loadInput(inputFile))
+      out ← managed(new PDDocument())
     } {
+      // Setup helper classes
       val layerUtility = new LayerUtility(out)
+      implicit val renderer = new PDFRenderer(in)
 
-      val renderer = new PDFRenderer(in)
-
-      def findInkBox(pageNo: Int) = {
-        val dpi = conf.dpi()
-        val marginOfError = (dpi / 72 * conf.inkMargin()) max 1
-        val threshold = 255 * conf.inkThreshold()
-
-        val img = renderer.renderImageWithDPI(pageNo, dpi, ImageType.GRAY)
-        //ImageIO.write(img, "PNG", new File("tmp.png"))
-        //val pdimg = LosslessFactory.createFromImage(out, img)
-        val rast = img.getData()
-        val pixelBuf = new Array[Int](rast.getNumBands)
-
-        var maxX = Float.MinValue
-        var maxY = Float.MinValue
-        var minX = Float.MaxValue
-        var minY = Float.MaxValue
-
-        for {
-          x <- 0 until rast.getWidth
-          y <- 0 until rast.getHeight
-          val v = rast.getPixel(x, y, pixelBuf)(0)
-          if v < threshold
-        } {
-          //println(x,y, v)
-          maxX = maxX max x
-          maxY = maxY max y
-          minX = minX min x
-          minY = minY min y
-        }
-
-        maxX = (maxX + marginOfError) min rast.getWidth
-        maxY = (maxY + marginOfError) min rast.getHeight
-        minX = (minX - marginOfError) max 0
-        minY = (minY - marginOfError) max 0
-
-        val scale = 72 / dpi
-        val r = new PDRectangle
-        // Y is flipped between raster and PDF
-        r.setLowerLeftX(minX * scale)
-        r.setLowerLeftY((rast.getHeight - maxY) * scale)
-        r.setUpperRightX(maxX * scale)
-        r.setUpperRightY((rast.getHeight - minY) * scale)
-        r
-      }
-
-      val inkBoxes = for (pageNo <- 0 until in.getNumberOfPages) yield findInkBox(pageNo)
+      // Get the ink boxes of each page
+      val inkBoxes = for (pageNo ← 0 until in.getNumberOfPages) yield findInkBox(pageNo)
+      // Compute the largest SIZE the can contain any of the boxes.
       val inkBoxSize = inkBoxes.map(_.createRetranslatedRectangle()).reduce(unionRect)
 
-      val shortEdgeMargin = conf.shortEdgeMargin().toFloat // 72f / 8
-      val longEdgeMargin = conf.longEdgeMargin().toFloat // 72f / 2
-      val interMargin = conf.interMargin().toFloat // 72f / 16
-
-      val pageSize = new PDRectangle(PDRectangle.LETTER.getHeight, PDRectangle.LETTER.getWidth)
-
-      val usedWidth = pageSize.getWidth - shortEdgeMargin * 2 - interMargin
-      val usedHeight = pageSize.getHeight - longEdgeMargin * 2
-
-      val scaleForWidth = usedWidth / (inkBoxSize.getWidth * 2)
-      val scaleForHeight = usedHeight / inkBoxSize.getHeight
+      // Compute the scale needed to fit the input pages perfectly in the width and height
+      val scaleForWidth = usedSize.getWidth / (inkBoxSize.getWidth * 2)
+      val scaleForHeight = usedSize.getHeight / inkBoxSize.getHeight
+      // Set scale to the minimum of the two so everything will fit
       val scale = scaleForHeight min scaleForWidth
+      
+      trace(0, s"Scale: ${scale.formatted("%.2f")}")
 
+      // Compute the margins needed to horizontally center everything properly for pages of size inkBoxSize
       val shortEdgeCenteringMargin = (pageSize.getWidth - inkBoxSize.getWidth * 2 * scale) / 3
       val interCenteringMargin = (pageSize.getWidth - inkBoxSize.getWidth * 2 * scale) / 3
 
-      for (pageNo <- 0 until in.getNumberOfPages by 2) {
+      // Iterate over pairs of pages
+      for (pageNo ← 0 until in.getNumberOfPages by 2) {
+        // Create and setup the output page
         val page = new PDPage()
         page.setMediaBox(pageSize)
-        //page.setRotation(90)
         out.addPage(page)
 
+        // Compute the actual ink box for this pair of pages based on the pair itself
         val inkBox = if (pageNo + 1 < in.getNumberOfPages)
           unionRect(inkBoxes(pageNo), inkBoxes(pageNo + 1))
         else
           inkBoxes(pageNo)
 
+        // Vertically centering margin
         val longEdgeCenteringMargin = (pageSize.getHeight - inkBox.getHeight * scale) / 2
 
-        def markInkBox(contentStream: PDPageContentStream) = {
-          contentStream.moveTo(inkBox.getLowerLeftX, inkBox.getLowerLeftY)
-          contentStream.lineTo(inkBox.getLowerLeftX, inkBox.getUpperRightY)
-          contentStream.lineTo(inkBox.getUpperRightX, inkBox.getUpperRightY)
-          contentStream.lineTo(inkBox.getUpperRightX, inkBox.getLowerLeftY)
-          contentStream.lineTo(inkBox.getLowerLeftX, inkBox.getLowerLeftY)
-        }
+        // Setup the drawing context for this page
+        for (contentStream ← managed(new PDPageContentStream(out, page, PDPageContentStream.AppendMode.APPEND, false))) {
+          // Iterate over the two input pages
+          for (subPage ← 0 to 1 if pageNo + subPage < in.getNumberOfPages) {
+            val n = pageNo + subPage
 
-        for (subPage <- 0 to 1 if pageNo + subPage < in.getNumberOfPages) {
-          val n = pageNo + subPage
-          val embPage = layerUtility.importPageAsForm(in, n)
-          //val inkBox = inkBoxes(n)
+            // Import the page as a Form XObject
+            val embPage = layerUtility.importPageAsForm(in, n)
 
-          for (contentStream <- managed(new PDPageContentStream(out, page, PDPageContentStream.AppendMode.APPEND, false))) {
             contentStream.saveGraphicsState();
 
+            // Compute the transform for this page
             val pageTransform = new Matrix()
+            // Translate to the corner of the page space
             pageTransform.translate(shortEdgeCenteringMargin, longEdgeCenteringMargin)
-            pageTransform.translate(subPage * interCenteringMargin, 0)
+            // Translate over by the inter-margin and the scaled page width based on which sub-page we are rendering.
+            pageTransform.translate(subPage * (interCenteringMargin + inkBoxSize.getWidth * scale), 0)
+            // Scale it
             pageTransform.scale(scale, scale)
+            // Translate so the corner of the ink on the page will be at 0,0
             pageTransform.translate(-inkBox.getLowerLeftX, -inkBox.getLowerLeftY)
-            pageTransform.translate(subPage * inkBoxSize.getWidth, 0)
 
             contentStream.transform(pageTransform)
 
             if (conf.drawBox()) {
-              markInkBox(contentStream)
+              markRectangle(contentStream, inkBox)
               contentStream.closeAndStroke()
             }
 
-            markInkBox(contentStream)
+            // Setup clipping for the page render
+            markRectangle(contentStream, inkBox)
             contentStream.clip()
-
+            // Draw the input page to the output
             contentStream.drawForm(embPage)
+
             contentStream.restoreGraphicsState()
           }
         }
       }
 
+      trace(0, s"Output file: $outputFile")
       out.save(outputFile)
     }
   }
 
   val degrees: Float = (Math.PI / 180).toFloat
 
+  /** Compute the union of two axis-aligned rectangle as a new AA rectangle.
+    */
   def unionRect(a: PDRectangle, b: PDRectangle): PDRectangle = {
     val r = new PDRectangle
 
@@ -218,28 +216,88 @@ object Main {
     r
   }
 
+  /** Open inputFile as a PDF converting from PS if needed.
+    */
   def loadInput(inputFile: File) = {
-    if (!(inputFile.isFile() && inputFile.canRead()))
-      throw new IOException("Input file not readable")
-
     try {
       PDDocument.load(inputFile)
     } catch {
-      case _: IOException => {
+      case _: IOException ⇒ {
         // Assume the PDF parse failed
         val psStream = try {
           new GZIPInputStream(new FileInputStream(inputFile))
         } catch {
-          case _: ZipException => {
+          case _: ZipException ⇒ {
             new FileInputStream(inputFile)
           }
         }
         import scala.sys.process._
         val pipeOutput = new PipedOutputStream()
         val pipeInput = new PipedInputStream(pipeOutput)
-        val proc = "ps2pdf - -" #< psStream #> pipeOutput run()
+        val proc = "ps2pdf - -" #< psStream #> pipeOutput run ()
         PDDocument.load(pipeInput)
       }
     }
+  }
+
+  /** Compute the AABB of the non-white areas on the page.
+    */
+  def findInkBox(pageNo: Int)(implicit conf: Conf, renderer: PDFRenderer) = {
+    val dpi = conf.dpi()
+    val marginOfError = (dpi / 72 * conf.inkMargin()) max 1
+    val threshold = 255 * conf.inkThreshold()
+
+    val img = renderer.renderImageWithDPI(pageNo, dpi, ImageType.GRAY)
+    //ImageIO.write(img, "PNG", new File("tmp.png"))
+    //val pdimg = LosslessFactory.createFromImage(out, img)
+    val rast = img.getData()
+    val pixelBuf = new Array[Int](rast.getNumBands)
+
+    var maxX = Float.MinValue
+    var maxY = Float.MinValue
+    var minX = Float.MaxValue
+    var minY = Float.MaxValue
+
+    for {
+      x ← 0 until rast.getWidth
+      y ← 0 until rast.getHeight
+      v = rast.getPixel(x, y, pixelBuf)(0)
+      if v < threshold
+    } {
+      //println(x,y, v)
+      maxX = maxX max x
+      maxY = maxY max y
+      minX = minX min x
+      minY = minY min y
+    }
+
+    maxX = (maxX + marginOfError) min rast.getWidth
+    maxY = (maxY + marginOfError) min rast.getHeight
+    minX = (minX - marginOfError) max 0
+    minY = (minY - marginOfError) max 0
+
+    val scale = 72 / dpi
+    val r = new PDRectangle
+    // Y is flipped between raster and PDF
+    r.setLowerLeftX(minX * scale)
+    r.setLowerLeftY((rast.getHeight - maxY) * scale)
+    r.setUpperRightX(maxX * scale)
+    r.setUpperRightY((rast.getHeight - minY) * scale)
+    r
+  }
+
+  /** Outline a rectangle to the content stream using moveTo/lineTo.
+    */
+  def markRectangle(contentStream: PDPageContentStream, box: PDRectangle) = {
+    contentStream.moveTo(box.getLowerLeftX, box.getLowerLeftY)
+    contentStream.lineTo(box.getLowerLeftX, box.getUpperRightY)
+    contentStream.lineTo(box.getUpperRightX, box.getUpperRightY)
+    contentStream.lineTo(box.getUpperRightX, box.getLowerLeftY)
+    contentStream.lineTo(box.getLowerLeftX, box.getLowerLeftY)
+  }
+  
+  def trace(level: Int, msg: => String)(implicit conf: Conf) {
+    if(level <= conf.verbosity)
+      println(msg)
   }
 }
